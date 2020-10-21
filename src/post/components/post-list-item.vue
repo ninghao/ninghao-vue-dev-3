9561467f462f2c4e1cdee41d4bf805cf5541009f
wanghao <wanghao@ninghao.net>
2020-10-21 10:37:19 +0800

    列表项目

---
+++ src/post/components/post-list-item.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="post-list-item">
+    <h3>{{ item.title }}</h3>
+    - <small>{{ item.user.name }}</small>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostListItem',
+
+  props: {
+    item: Object,
+  },
+};
+</script>
--- src/post/components/post-list.vue
+++ src/post/components/post-list.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="post-list">
-    <div class="post-list-item" v-for="item in posts" :key="item.id">
-      <h3>{{ item.title }}</h3>
-      - <small>{{ item.user.name }}</small>
-    </div>
+    <PostListItem v-for="item in posts" :key="item.id" :item="item" />
   </div>
 </template>
 
 <script>
 import { apiHttpClient } from '@/app/app.service';
+import PostListItem from './post-list-item.vue';
 
 export default {
   data() {
@@ -34,6 +32,10 @@ export default {
         console.log(error);
       }
     },
+  },
+
+  components: {
+    PostListItem,
   },
 };
 </script>
